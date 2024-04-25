16cd1c2c7804039183b153eea39ce9536855c227
xCk27x <pdes0722@gmail.com>
2024-04-25 11:08:36 +0800

    Add necessary configuration files and dependencies

---
+++ .eslintrc.cjs
@@ -0,0 +1,17 @@
+/* eslint-disable prettier/prettier */
+module.exports = {
+  env: {
+    browser: true,
+    es2023: true
+  },
+  extends: ["@nuxtjs/eslint-config-typescript", "prettier"],
+  parserOptions: {
+    ecmaVersion: 2023,
+    sourceType: "module"
+  },
+  plugins: ["prettier"],
+  rules: {
+    "no-undef": "off",
+    "prettier/prettier": ["error", { "endOfLine": "auto" }]
+  },
+};
---
+++ nuxt.config.ts
@@ -0,0 +1,13 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ["@pinia/nuxt", "@nuxtjs/tailwindcss"],
+  imports: {
+    dirs: [
+      // Scan top-level modules
+      'composables',
+      // ... or scan modules nested one level deep with a specific name and file extension
+      'composables/*/index.{ts,js,mjs,mts}',
+    ]
+  }
+});
